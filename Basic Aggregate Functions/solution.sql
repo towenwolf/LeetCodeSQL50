51c7f9d13b7a5b8184c7f3e7244ebe5d62152b68
Travis Wolf <travis.wolf@coldist.com>
2024-09-20 10:50:44 -0700

    final solution

--- Basic Aggregate Functions/solution.sql
+++ Basic Aggregate Functions/solution.sql
@@ -1,6 +1,10 @@
 USE sbx
 GO
 
-SELECT AVG(price)
-FROM Prices
-GROUP BY [product_id]
+SELECT p.product_id, ROUND((SUM(CONVERT(DECIMAL(10,2),price) * CONVERT(DECIMAL(10,2), units)) / SUM(CONVERT(DECIMAL(10,2), units))), 2) AS average_price
+FROM Prices p
+LEFT JOIN UnitsSold u
+    ON p.product_id = u.product_id
+    AND u.purchase_date BETWEEN p.start_date AND p.end_date
+GROUP BY p.product_id
+
