b638ed8f9ff0900abd6d080685f3cea3ec62ada4
Travis Wolf <travis.wolf@coldist.com>
2024-09-25 11:05:31 -0700

    adding simpler solution

--- Sorting and Grouping/solution.sql
+++ Sorting and Grouping/solution.sql
@@ -3,10 +3,14 @@
 USE sbx
 GO
 
-SELECT teacher_id, COUNT(subject_id) AS cnt
-FROM (
-SELECT teacher_id, subject_id
+-- SELECT teacher_id, COUNT(subject_id) AS cnt
+-- FROM (
+-- SELECT teacher_id, subject_id
+-- FROM Teacher
+-- GROUP BY teacher_id, subject_id
+-- ) a
+-- GROUP BY teacher_id
+
+SELECT teacher_id,  COUNT(DISTINCT subject_id) AS cnt
 FROM Teacher
-GROUP BY teacher_id, subject_id
-) a
 GROUP BY teacher_id
