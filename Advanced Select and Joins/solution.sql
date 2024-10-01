2fef7bdb26297deee5a63a9b3725934b38516457
Travis Wolf <travis.wolf@coldist.com>
2024-10-01 09:23:30 -0700

    1978

--- Advanced Select and Joins/solution.sql
+++
@@ -1,8 +0,0 @@
-USE sbx
-GO
-
-SELECT e.employee_id, COUNT(*) AS reports_count, AVG(ee.age) AS average_age
-FROM Employees e
-JOIN Employees ee 
-    ON e.employee_id = ee.reports_to
-GROUP BY e.employee_id
---
+++ Subqueries/1978-sql-schema.sql
@@ -0,0 +1,18 @@
+USE sbx
+GO
+
+IF EXISTS (SELECT * FROM sys.tables WHERE name = 'Employees')
+BEGIN
+    DROP TABLE Employees;
+    CREATE TABLE Employees (
+        employee_id int, name varchar(20), manager_id int, salary int
+    );
+END;
+
+Truncate table Employees
+insert into Employees (employee_id, name, manager_id, salary) values ('3', 'Mila', '9', '60301')
+insert into Employees (employee_id, name, manager_id, salary) values ('12', 'Antonella', NULL, '31000')
+insert into Employees (employee_id, name, manager_id, salary) values ('13', 'Emery', NULL, '67084')
+insert into Employees (employee_id, name, manager_id, salary) values ('1', 'Kalel', '11', '21241')
+insert into Employees (employee_id, name, manager_id, salary) values ('9', 'Mikaela', NULL, '50937')
+insert into Employees (employee_id, name, manager_id, salary) values ('11', 'Joziah', '6', '28485')
